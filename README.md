d445b895174d93b1587f03672c7523eda7b9d738
I Gusti Kade Sugiantara <sugiantara.igk@gmail.com>
2023-07-30 09:39:47 +0700

    add try catch utils

---
+++ README.md
@@ -0,0 +1,7 @@
+# Javascript Utilities Function
+
+#### Function list
+
+1. isEmptyOrNull
+2. isEmptyArray
+3. tryTo
--- src/common/index.js
+++ src/common/index.js
@@ -1,8 +1,32 @@
+/**
+ * Check if value is null or undefined or empty string
+ * @param any-value
+ * @returns {boolean}
+ */
 export function isEmptyOrNull(value) {
   const checkForValue = ["", " ", undefined, null];
   return checkForValue.includes(value);
 }
 
-export function isEmptyArray(array = []) {
-  return array.length === 0;
+/**
+ * Check if array is empty or not
+ * @param array
+ * @returns {boolean}
+ */
+export function isEmptyArray(array) {
+  return Array.isArray(array) && array.length === 0;
+}
+
+/**
+ * To call async function with try catch block
+ * @param async-function
+ * @returns [result, error]
+ */
+export async function tryTo(action) {
+  try {
+    const result = await action();
+    return [result, null];
+  } catch (error) {
+    return [null, error];
+  }
 }
